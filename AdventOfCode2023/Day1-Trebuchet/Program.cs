40c97c53c02e5ddf76010b9da4ab467255f39b6f
Michael <mick_1402@hotmail.com>
2023-12-01 22:29:16 -0500

    Added second step tp calibration

--- AdventOfCode2023/Day1-Trebuchet/Program.cs
+++ AdventOfCode2023/Day1-Trebuchet/Program.cs
@@ -2,16 +2,32 @@
 
 using System.Text.RegularExpressions;
 
+var conversionMapper = new Dictionary<string, string>
+{
+    { "one", "1" },
+    { "two", "2" },
+    { "three", "3" },
+    { "four", "4" },
+    { "five", "5" },
+    { "six", "6" },
+    { "seven", "7" },
+    { "eight", "8" },
+    { "nine", "9" },
+};
+
 var digits = CalibrationFinderRegex();
-// https://regex101.com/r/1wW6Tq/1
 var digitsAndWords = Calibrationv2FinderRegex();
 
 var entries = File.ReadLines(args[0]);
 
 var calibrationValues = entries.Select(GetCalibration);
+var calibrationV2Values = entries.Select(GetCalibrationWithWordConversion);
 
-Console.WriteLine("Calibration value:");
+Console.WriteLine("Calibration value version 1:");
 Console.WriteLine(calibrationValues.Sum());
+
+Console.WriteLine("Calibration value version 2:");
+Console.WriteLine(calibrationV2Values.Sum());
 Console.ReadKey();
 
 // Get calibration value from string entry
@@ -27,15 +43,31 @@ int GetCalibration(string entry)
     return int.TryParse(numberStr, out var calibration) ? calibration : 0;
 }
 
-int GetCalibration2(string entry)
+int GetCalibrationWithWordConversion(string entry)
 {
+    var match = digitsAndWords.Match(entry);
 
+    var firstValue = match.Groups["first"].Value;
+    var lastValue = match.Groups["last"].Value;
+
+    var first = conversionMapper.TryGetValue(firstValue, out var v) ? v : firstValue;
+    var last = conversionMapper.TryGetValue(lastValue, out var w) ? w : lastValue;
+
+    var numberStr = first + last;
+
+    if (numberStr.Length == 1)
+        numberStr += numberStr;
+
+    return int.TryParse(numberStr, out var calibration) ? calibration : 0;
 }
 
 partial class Program
 {
     [GeneratedRegex(@"\D*(?<first>\d)?.*(?<last>\d)\D*")]
     private static partial Regex CalibrationFinderRegex();
-    [GeneratedRegex(@"(?<first>one|two|three|four|five|six|seven|eight|nine|\d)?.*(?<last>one|two|three|four|five|six|seven|eight|nine|\d).*")]
+
+    //https://regex101.com/r/nt98t2/1
+    [GeneratedRegex(@"(?<first>one|two|three|four|five|six|seven|eight|nine|\d)(.*(?<last>one|two|three|four|five|six|seven|eight|nine|\d))?")]
     private static partial Regex Calibrationv2FinderRegex();
 }
+
